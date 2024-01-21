5c0cca4b0551f45388aecea9895c252f631a059e
Marina <marinareis.mr@gmail.com>
2024-01-21 09:07:08 -0300

    tirando varias functions e criando uma só

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
     console.log('O botão foi clicado');

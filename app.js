caabe0ec64de009b1b21405c406ab3288b1fb9a5
Marina <marinareis.mr@gmail.com>
2024-01-21 09:06:14 -0300

    inciando com functions

---
+++ app.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+
+
+function verificarChute(){
+    console.log('O botão foi clicado');
+}

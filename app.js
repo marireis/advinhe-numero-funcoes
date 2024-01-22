67f7958c262acd2847c98d56e2b4c9fef7662fec
Marina <marinareis.mr@gmail.com>
2024-01-22 11:02:30 -0300

    função de condição para o chute

--- app.js
+++ app.js
@@ -11,11 +11,18 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
     let chute= document.querySelector('input').value
-    console.log(chute == numeroSecreto);
-    console.log(`${numeroSecreto}`);
-    saudacao();
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1','Acertou');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto');       
+    } else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O numero secreto é menor');
+        } else{
+            exibirTextoNaTela('p', 'O numero secreto é maior');
+        }
+    }
 }
-
+    
 function gerarNumAleatorio(){
    return parseInt(Math.random() * 10 + 1)
 }
